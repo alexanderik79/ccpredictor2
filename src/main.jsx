febdd5c7cb8ae3054876563388643d061d3e8331
alexanderik79 <alexanderik79@gmail.com>
2025-09-27 20:24:58 +0200

    jsx

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+// src/main.jsx (ФИНАЛЬНАЯ ВЕРСИЯ)
+
+import React, { StrictMode } from 'react'; // <-- ИЗМЕНЕНИЕ ЗДЕСЬ
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    createRoot(rootElement).render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+}
